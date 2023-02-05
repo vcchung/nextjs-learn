9a18f62ed5a063b43f81a34fae9b3b37f52abc3f
vcchung <vcchung@gmail.com>
2023-02-05 14:47:17 +0800

    update

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,32 @@
 import Link from "next/link";
-
+import { FaHome, FaListUl, FaPlus } from "react-icons/fa";
 function NavBar() {
   return (
     <ul className="flex flex-row m-3 text-white bg-blue-900 px-10 py-3 font-bold border-collapse border-spacing-3">
       <li>
-        <Link className="m-3" href="/">
-          Home
+        <Link className="m-3 px-10" href="/">
+          <FaHome className="font-extrabold" />
         </Link>
       </li>
       <li>
-        <Link href="/list-tasks-csr" className="m-3">
-          List Tasks Client Side Rendering
+        <Link href="/list-tasks-csr" className="m-3 px-10">
+          <FaListUl /> Client Side Rendering
         </Link>
       </li>
       <li>
-        <Link href="/list-tasks-static-props" className="m-3">
-          List Tasks Static Props
+        <Link href="/list-tasks-static-props" className="m-3 px-10">
+          <FaListUl /> Static Props
         </Link>
       </li>
       <li>
-        <Link href="/list-tasks-server-props" className="m-3">
-          List Tasks Server Side Props
+        <Link href="/list-tasks-server-props" className="m-3 px-10">
+          <FaListUl /> Server Side Props
         </Link>
       </li>
       <li>
-        <Link href="/new-tasks">New</Link>
+        <Link href="/new-tasks" className="px-10 m-3">
+          <FaPlus />
+        </Link>
       </li>
     </ul>
   );
